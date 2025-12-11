058c7fe59962b179ee787040fe5040cb09021a22
yuanhsihsu <yuanhsi0727@gmail.com>
2025-12-11 15:57:02 +0800

    week05

---
+++ week05/week05-5a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int now;
+    cin >> now;
+    int M = now, m=now;
+    for (int i=0; i<5; i++)
+    {
+        cin >> now;
+        if (now>M) M=now;
+        if (now<m) m=now;
+    }
+    cout << M-m;
+}
